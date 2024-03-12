24a18fec27bc3c1894f60df001e7870c349d55c2
Bernardthe3rd <bammeijer@outlook.com>
2024-03-12 22:44:47 +0100

    finished the rest

--- src/pages/DetailBlogpost.jsx
+++ src/pages/DetailBlogpost.jsx
@@ -1,30 +1,27 @@
 import {Link, useParams} from "react-router-dom";
 import posts from "../constants/data.json"
-// import {dateFormat} from "../helpers/dateFormat.js";
+import {dateFormat} from "../helpers/dateFormat.js";
 
 const DetailBlogpost = () => {
     const { id} = useParams();
-    console.log(posts[0])
-    // const postsId = posts.map(post => {
-    //     return post.id.toString()
-    // })
 
-    // const isIdinArray = postsId.includes(id);
-    // console.log(posts[id].title)
+    const newId = posts.find(post => {
+        return post.id.toString() === id;
+    })
+    console.log(newId)
+
 
     return (
         <>
-            <p>{id}</p>
-            <article className={"outer-container"}>
-                <h1>{posts[id].title}</h1>
-                <h2>{posts[id].subtitle}</h2>
-                <p>Geschreven door {posts[id].author} op {posts[id].created}</p>
-                <p>{posts[id].content}</p>
-                <p>{posts[id].comments} reactie - {posts[id].shares} keer gedeeld</p>
-                <Link to={"/posts"}>Terug naar de overzichtpagina</Link>
+            {/*<p>{id}</p>*/}
+            <article className={""}>
+                <h1>{newId.title}</h1>
+                <h2>{newId.subtitle}</h2>
+                <p>Geschreven door {newId.author} op {dateFormat(newId.created)}</p>
+                <p>{newId.content}</p>
+                <p>{newId.comments} reactie - {newId.shares} keer gedeeld</p>
+                <Link to={"/posts"} className={"link-title"}>Terug naar de overzichtpagina</Link>
             </article>
-
-            {/*{isIdinArray ? (<p>hij zit erin {posts[id].title}</p>) : (<p>hij zit er niet in</p>)}*/}
         </>
     );
 };
